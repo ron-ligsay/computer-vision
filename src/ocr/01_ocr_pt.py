8415cbc643b7073239a22e03854e8e18e3348749
ron-ligsay <akyligsay16@gmail.com>
2023-08-27 18:38:38 +0800

    using pil

--- src/ocr/01_ocr_pt.py
+++ src/ocr/01_ocr_pt.py
@@ -1,3 +1,16 @@
 import cv2
 import PIL
 import pytesseract
+
+from PIL import Image
+
+PATH = "C:\\Users\\aky\\Documents\\Programs\\computer-vision\\"
+im_file = PATH + "data\\pdf\\page_01.jpg"
+
+im = Image.open(im_file)
+print(im)
+print(im.size)
+im.show()
+
+# saving a file
+# im.save("C:\\Users\\aky\\Documents\\Programs\\computer-vision\\src\\ocr\\page01.png")
